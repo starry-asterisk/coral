e1aa39e6f209bbb4a0b05c8acebe9f65fd646bde
83 <83@83-PC>
2019-12-12 15:29:44 +0900

    delete

--- coral/src/main/java/com/coral/www/User/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.coral.www.User;
-
-public interface UserService {
-	public UserDTO getInfo(UserDTO dto);
-	public UserDTO login(UserDTO dto);
-	public UserDTO insertHistory(UserDTO dto);
-}
